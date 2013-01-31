85c5b7a10d2d8d86aa8016fd0f0c9f31e9b8fa1b
" <d>
2013-01-31 10:14:19 +0000

    initial one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thicketee::Application.config.secret_token = 'd5efe2c264523e1d520be73502c2e78630a7946ace62949684c69f271624c51c898f8373b50312153b83605d6a08db385a98a646d6dbf8778ef508ff2232bbb9'
